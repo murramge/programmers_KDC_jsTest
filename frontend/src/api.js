42e9aa06c283bb65f15d86c42998316e7b437234
murramge <dmdgi37@naver.com>
2024-01-19 20:59:48 +0900

    로딩 구현

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,25 @@
 const API_ENDPOINT = "http://localhost:4001";
-
+const $loadingSpineer = document.querySelector(".loading-spinner");
+let loading = false;
 const api = {
   fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
-    );
+    if ($loadingSpineer.className.includes("v-none")) {
+      $loadingSpineer.className = $loadingSpineer.className.replace(
+        "v-none",
+        "v-show"
+      );
+    }
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`)
+      .then((res) => {
+        return res.json();
+      })
+      .finally(() => {
+        if ($loadingSpineer.className.includes("v-show")) {
+          $loadingSpineer.className = $loadingSpineer.className.replace(
+            "v-show",
+            "v-none"
+          );
+        }
+      });
   },
 };
