7ec301844ef631fd742d8bec624df225584bd262
murramge <dmdgi37@naver.com>
2024-01-19 15:24:18 +0900

    프로젝트셋팅

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routes')
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
 // Make an express app
-const app = express()
-const port = 4001
+const app = express();
+const port = 4001;
 
 // Use middlewares
-app.use(cors())
-app.use(express.static(`${__dirname}/public`))
-app.use('/api', routes)
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+app.use("/api", routes);
 
-app.listen(port, () => console.log(`Express app is running on port: ${port}`))
+app.listen(port, () => console.log(`Express app is running on port: ${port}`));
 
-module.exports = app
+module.exports = app;
