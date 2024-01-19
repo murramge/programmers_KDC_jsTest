6c92b20b8d706a628fd48d68e93fd541cf511029
murramge <dmdgi37@naver.com>
2024-01-19 21:17:08 +0900

    검색결과없음 ui 구현

--- frontend/src/SearchResult.js
+++ frontend/src/SearchResult.js
@@ -2,10 +2,12 @@ class SearchResult {
   $searchResult = null;
   data = null;
   onClick = null;
+  $searchResultText = null;
 
   constructor({ $target, initialData, onClick }) {
     this.$searchResult = document.createElement("div");
     this.$searchResult.className = "SearchResult";
+    this.$searchResultText = document.querySelector(".search-result-text");
     $target.appendChild(this.$searchResult);
 
     this.data = initialData;
@@ -15,25 +17,35 @@ class SearchResult {
   }
 
   setState(nextData) {
-    this.data = nextData;
+    if (nextData.length) {
+      this.data = nextData;
+    } else {
+      if (this.$searchResultText.className.includes("v-none")) {
+        this.$searchResultText.className =
+          this.$searchResultText.className.replace("v-none", "v-show");
+      }
+    }
     this.render();
   }
 
   render() {
-    this.$searchResult.innerHTML = this.data
-      .map(
-        cat => `
+    const datas = this.data;
+    if (datas.length) {
+      this.$searchResult.innerHTML = this.data
+        .map(
+          (cat) => `
           <div class="item">
             <img src=${cat.url} alt=${cat.name} />
           </div>
         `
-      )
-      .join("");
+        )
+        .join("");
 
-    this.$searchResult.querySelectorAll(".item").forEach(($item, index) => {
-      $item.addEventListener("click", () => {
-        this.onClick(this.data[index]);
+      this.$searchResult.querySelectorAll(".item").forEach(($item, index) => {
+        $item.addEventListener("click", () => {
+          this.onClick(this.data[index]);
+        });
       });
-    });
+    }
   }
 }
